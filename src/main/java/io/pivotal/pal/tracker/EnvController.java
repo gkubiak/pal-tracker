37208270ebedc19ab76fceef4805ec12146a855c
Glenn Kubiak <13876530+gkubiak@users.noreply.github.com>
2018-06-04 15:19:12 -0600

    done through manifest

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,40 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    String port;
+    String memory_limit;
+    String cf_instance_index;
+    String cf_instance_addr;
+
+    public EnvController(
+        @Value("${PORT:NOT SET}") String port,
+        @Value("${MEMORY_LIMIT:NOT SET}") String memory_limit,
+        @Value("${CF_INSTANCE_INDEX:NOT SET}") String cf_instance_index,
+        @Value("${CF_INSTANCE_ADDR:NOT SET}") String cf_instance_addr
+    ) {
+        this.port = port;
+        this.memory_limit = memory_limit;
+        this.cf_instance_index = cf_instance_index;
+        this.cf_instance_addr = cf_instance_addr;
+    }
+
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> someMap = new HashMap<String, String>();
+        someMap.put("PORT", port);
+        someMap.put("MEMORY_LIMIT", memory_limit);
+        someMap.put("CF_INSTANCE_INDEX", cf_instance_index);
+        someMap.put("CF_INSTANCE_ADDR", cf_instance_addr);
+        return someMap;
+    }
+}
